d07ccb9e224ea41c03c1c0872b750e449297098d
rdmclin2 <rdmclin2@163.com>
2016-01-26 16:58:25 +0800

    remove assets

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(cors());
 
-app.use('/assets',express.static(path.join(__dirname, 'assets')));
+//app.use('/assets',express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
